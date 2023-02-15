b2b5f53580824f3d17d7aa3d83c0095f9e660a22
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-15 14:29:17 +0300

    Update fork.c + .gitignore

--- shell_practice/0x00-shell/fork.c
+++ shell_practice/0x00-shell/fork.c
@@ -1,16 +1,29 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
 
 int main(void)
 {
     pid_t pid;
 
-    printf("Before fork\n");
+    printf("Before fork:\n");
+    printf("PID = %u, PPID = %u\n", getpid(), getppid());
 
     pid = fork();
     if (pid == -1)
         perror("fork() failure");
 
-    printf("After fork\n");
+    if (pid == 0)
+    {
+        printf("Before sleep:\nPID = %u, PPID = %u\n", getpid(), getppid());
+        sleep(10);
+        printf("Child process:\n");
+        printf("After sleep:\nPID = %u, PPID = %u\n", getpid(), getppid());
+    }
+
+    else
+    {
+        printf("Parent process exiting...\n");
+    }
     return (0);
 }
