9e2db4a8beef0113ce00a85d69a8f045bc4c3b25
FourtyThree43 <shaqmwa@outlook.com>
2023-02-20 22:03:48 +0300

    simple shell Header and prototypes

---
+++ shell.h
@@ -0,0 +1,33 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* string.c */
+int _strlen(char *);
+int _strcmp(char *, char *);
+char *starts_with(const char *, const char *);
+char *_strcat(char *, char *);
+
+/* string1.c */
+char *_strcpy(char *, char *);
+char *_strdup(const char *);
+void _puts(char *);
+int _putchar(char);
+
+/* mem_mgt.c */
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *dest, char *src, unsigned int n);
+void *_realloc(void *, unsigned int, unsigned int);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
