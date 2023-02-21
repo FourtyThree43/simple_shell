3c5a3455b03215c17dda7d0cc97e0f3b25c07235
Kemboiray <113639995+Kemboiray@users.noreply.github.com>
2023-02-21 13:09:19 +0300

    Update remote

--- shell_practice/0x00-shell/14-_setenv2.c
+++ shell_practice/0x00-shell/14-_setenv2.c
@@ -1,5 +1,5 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -15,86 +15,94 @@ extern char **environ;
  */
 int _setenv(const char *name, const char *value, int overwrite)
 {
-  if (name == NULL || value == NULL)
-  {
-    return -1;
-  }
+	if (name == NULL || value == NULL)
+	{
+		return -1;
+	}
 
-  // Check if the environment variable already exists
-  int i = 0;
-  while (environ[i] != NULL)
-  {
-    if (strncmp(name, environ[i], strlen(name)) == 0 && environ[i][strlen(name)] == '=')
-    {
-      if (overwrite)
-      {
-        // Overwrite the existing variable
-        size_t new_env_len = strlen(name) + 1 + strlen(value) + 1;
-        char *new_env = malloc(new_env_len);
-        if (new_env == NULL)
-        {
-          return -1;
-        }
-        strcpy(new_env, name);
-        strcat(new_env, "=");
-        strcat(new_env, value);
-        free(environ[i]);
-        environ[i] = new_env;
-      }
-      return 0;
-    }
-    i++;
-  }
+	// Check if the environment variable already exists
+	int i = 0;
+	while (environ[i] != NULL)
+	{
+		if (strncmp(name, environ[i], strlen(name)) == 0 && environ[i][strlen(name)] == '=')
+		{
+			if (overwrite)
+			{
+				// Overwrite the existing variable
+				size_t new_env_len = strlen(name) + 1 + strlen(value) + 1;
+				char *new_env = malloc(new_env_len);
+				if (new_env == NULL)
+				{
+					return -1;
+				}
+				strcpy(new_env, name);
+				strcat(new_env, "=");
+				strcat(new_env, value);
+				free(environ[i]);
+				environ[i] = new_env;
+			}
+			return 0;
+		}
+		i++;
+	}
 
-  // Add a new environment variable
-  size_t new_env_len = strlen(name) + 1 + strlen(value) + 1;
-  char *new_env = malloc(new_env_len);
-  if (new_env == NULL)
-  {
-    return -1;
-  }
-  strcpy(new_env, name);
-  strcat(new_env, "=");
-  strcat(new_env, value);
+	// Add a new environment variable
+	size_t new_env_len = strlen(name) + 1 + strlen(value) + 1;
+	char *new_env = malloc(new_env_len);
+	if (new_env == NULL)
+	{
+		return -1;
+	}
+	strcpy(new_env, name);
+	strcat(new_env, "=");
+	strcat(new_env, value);
 
-  // Allocate a new environ array with space for the new variable
-  int environ_size = i;
-  char **new_environ = malloc(sizeof(char *) * (environ_size + 2));
-  if (new_environ == NULL)
-  {
-    free(new_env);
-    return -1;
-  }
+	// Allocate a new environ array with space for the new variable
+	int environ_size = i;
+	char **new_environ = malloc(sizeof(char *) * (environ_size + 2));
+	if (new_environ == NULL)
+	{
+		free(new_env);
+		return -1;
+	}
 
-  // Copy the existing environ array into the new array
-  for (int j = 0; j < environ_size; j++)
-  {
-    new_environ[j] = environ[j];
-  }
+	// Copy the existing environ array into the new array
+	for (int j = 0; j < environ_size; j++)
+	{
+		new_environ[j] = environ[j];
+	}
 
-  // Add the new variable to the new array
-  new_environ[environ_size] = new_env;
-  new_environ[environ_size + 1] = NULL;
+	// Add the new variable to the new array
+	new_environ[environ_size] = new_env;
+	new_environ[environ_size + 1] = NULL;
 
-  // Set the global environ variable to the new array
-  environ = new_environ;
+	// Set the global environ variable to the new array
+/*	for (i = 0; environ[i]; i++)
+		free(environ[i]);
+	free(environ);
+*/	environ = new_environ;
 
-  return 0;
+	return 0;
 }
 
 // C program to illustrate setenv function
 int main()
 {
- char *pth;
- if(_setenv("INCLUDE","usr/nto//include:/home/scholar/include",1)==0)
-   {
-     if((pth=getenv("INCLUDE"))!=NULL)
-      printf("\n\n\tINCLUDE:%s\n\n\t",pth);
-   }
-  else
-  {
-  	printf("\n\n\tError! No such Environment Value Exists.");
-  }
-  
-  return 0;
+	char *pth;
+	int i;
+	if(_setenv("INCLUDE","usr/nto//include:/home/scholar/include",1)==0)
+		{
+			if((pth=getenv("INCLUDE"))!=NULL)
+				printf("\n\n\tINCLUDE:%s\n\n\t",pth);
+			free(pth - 8);
+		}
+	else
+	{
+			printf("\n\n\tError! No such Environment Value Exists.");
+	}
+
+/*	for (i = 0; environ[i]; i++)
+		free(environ[i]);
+*/	free(environ);
+	return 0;
 }
